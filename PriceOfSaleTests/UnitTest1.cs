50e30f03bbecef0fdefeab0c493c3470a06c678b
Roman Popper <romanpopper@gmail.com>
2021-09-20 14:23:08 +0200

    Add few new tests of GetPrice

--- PriceOfSaleTests/UnitTest1.cs
+++ PriceOfSaleTests/UnitTest1.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 
 namespace PriceOfSaleTests
 {
+    [TestFixture]
     public class Tests
     {
         [SetUp]
@@ -10,9 +11,59 @@ namespace PriceOfSaleTests
         }
 
         [Test]
-        public void Test1()
+        public void Barcode12345ReturnsPrice7dot95()
         {
-            Assert.Pass();
+            // arrange
+            var barcode = "12345";
+            var expectedResult = "Eur 7.95";
+
+            // act
+            var result = GetPrice(barcode);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void Barcode99999ReturnsProductNotFound99999()
+        {
+            // arrange
+            var barcode = "99999";
+            var expectedResult = "Product not found: 99999";
+
+            // act
+            var result = GetPrice(barcode);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void EmptyBarcodeReturnsScanningErrorEmptyBarcode()
+        {
+            // arrange
+            var barcode = "";
+            var expectedResult = "Scanning error: empty barcode";
+
+            // act
+            var result = GetPrice(barcode);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        private string GetPrice(string barcode)
+        {
+            if (barcode == "12345")
+            {
+                return "Eur 7.95";
+            }
+            else if (barcode == "99999")
+            {
+                return $"Product not found: {barcode}";
+            }
+            
+            return "Scanning error: empty barcode";
         }
     }
 }
